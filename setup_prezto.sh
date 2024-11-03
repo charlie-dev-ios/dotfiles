61172f8ad4b35bbbaf1641066f93427822428783
charlie-dev-ios <py.yeronga@gmail.com>
2024-11-03 12:09:47 +0900

    prezto周りの設定を整理

--- setup_prezto.sh
+++ setup_prezto.sh
@@ -7,18 +7,15 @@ set -Ceu
 BASEDIR=$(dirname $0)
 cd $BASEDIR
 
-# prezto関係の設定
-ln -snfv ${PWD}/.p10k.zsh ~/.p10k.zsh
+# prezto自体のインストール
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-cd .prezto/runcoms
-runcoms=`find ${PWD} -name ".*" -type f -maxdepth 1`
+# prezto関係の設定
+runcoms=`find ${PWD}/prezto -name ".*" -type f -maxdepth 1`
 for file in $runcoms; do
     # シンボリックリンクを貼る
     ln -snfv $file ~
-    # prezto自体にもシンボリックリンクを貼る必要がある
-    ln -snfv $file ~/.zprezto/runcoms
 done
 
 # p10k用のフォントのインストール
-cd ../../
-cp -r fonts/ ~/Library/Fonts
+cp -r ./fonts/ ~/Library/Fonts
