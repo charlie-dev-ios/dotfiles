2126a4f70367247616a27e2c3c2c4f45dc7a0fb2
charlie-dev-ios <py.yeronga@gmail.com>
2024-11-03 12:18:25 +0900

    preztoのインストールを毎回実行しなくていいように修正

--- setup_prezto.sh
+++ setup_prezto.sh
@@ -8,7 +8,9 @@ BASEDIR=$(dirname $0)
 cd $BASEDIR
 
 # prezto自体のインストール
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+if ! [ -d "${ZDOTDIR:-$HOME}/.zprezto" ]; then
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+fi
 
 # prezto関係の設定
 runcoms=`find ${PWD}/prezto -name ".*" -type f -maxdepth 1`
