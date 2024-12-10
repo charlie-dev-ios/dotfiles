58911297bed2dad8810cce624575c50e6be759b4
charlie-dev-ios <py.yeronga@gmail.com>
2024-12-10 23:00:30 +0900

    vimにauto-sessionをインストール

---
+++ vim/lua/plugins/auto-session.lua
@@ -0,0 +1,22 @@
+return {
+  'rmagatti/auto-session',
+  lazy = false,
+  keys = {
+      { "<Leader>ws", "<cmd>SessionSave<CR>" },
+      { "<Leader>wr", "<cmd>SessionRestore<CR>" },
+  },
+  config = function()
+      local auto_session = require("auto-session")
+
+      local function restore_nvim_tree()
+          require("nvim-tree.api").tree.toggle({ focus = false })
+      end
+
+      auto_session.setup({
+          suppressed_dirs = { 
+              '~/',
+          },
+          post_restore_cmds = { restore_nvim_tree },
+      })
+  end,
+}
