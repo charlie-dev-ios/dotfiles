bab228cdc73aa7df7c0037a6abfd7f22fb182820
charlie-dev-ios <py.yeronga@gmail.com>
2024-12-10 23:02:13 +0900

    auto-sessionの導入に伴ってnvim-treeの設定を変更
    * そのままだとauto-sessionがうまく動かない問題を修正
    * nvim-treeのキーマップを修正

--- vim/lua/plugins/nvim-tree.lua
+++ vim/lua/plugins/nvim-tree.lua
@@ -1,34 +1,44 @@
 return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
-  lazy = false,
-  priority = 1000,
   dependencies = {
     "nvim-tree/nvim-web-devicons",
+  },
+  -- nvim-treeはvim .をvim NeovimTree_1に書き換えてしまう
+  -- そのためauto-sessionが正常に動作しなくなる
+  -- 対応策として本来はLazyLoadするべきではないが遅延読み込みを行っている
+  cmd = {
+    'NvimTreeOpen',
+    'NvimTreeClose',
+    'NvimTreeToggle',
+    'NvimTreeFocus',
+    'NvimTreeRefresh',
+    'NvimTreeFindFile',
+    'NvimTreeFindFileToggle',
+    'NvimTreeClipboard',
+    'NvimTreeResize',
+    'NvimTreeCollapse',
+    'NvimTreeCollapseKeepBuffers',
+    'NvimTreeHiTest',
+  },
+  keys = {
+      { "<Leader>ee", "<cmd>NvimTreeToggle<CR>" },
+      { "<Leader>ef", "<cmd>NvimTreeFindFile<CR>" },
+      { "<Leader>er", "<cmd>NvimTreeRefresh<CR>" },
   },
   init = function()
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
   end,
-  config = function()
-      local nvim_tree = require("nvim-tree")
-      nvim_tree.setup({
-        sort = {
-            sorter = "case_sensitive",
-        },
-        view = {
-            width = 35,
-        },
-        filters = {
-            dotfiles = true,
-        }
-      })
-
-      local opts = { noremap = true, silent = true }
-      local keymap = vim.keymap
-      keymap.set("n", "<Leader>tt", "<cmd>NvimTreeToggle<CR>", opts)
-      keymap.set("n", "<Leader>to", "<cmd>NvimTreeOpen<CR>", opts)
-      keymap.set("n", "<Leader>tc", "<cmd>NvimTreeClose<CR>", opts)
-      keymap.set("n", "<Leader>tf", "<cmd>NvimTreeFocus<CR>", opts)
-  end,
+  opts = {
+    sort = {
+        sorter = "case_sensitive",
+    },
+    view = {
+        width = 35,
+    },
+    filters = {
+        dotfiles = false,
+    }
+  },
 }
