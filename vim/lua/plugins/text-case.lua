2e1cad70159ba997536a1d6bd2acb3c26970de4b
charlie-dev-ios <py.yeronga@gmail.com>
2024-12-10 23:54:56 +0900

    text-caseのキーマップの書き方を変更
    TextCaseのメニューをTelescopeで開くキーマップを追加

--- vim/lua/plugins/text-case.lua
+++ vim/lua/plugins/text-case.lua
@@ -6,13 +6,14 @@ return {
     require("telescope").load_extension("textcase")
 
     local opts = { noremap = true, silent = true }
-    vim.keymap.set("n", "gas", "<cmd>:lua require('textcase').current_word('to_snake_case')<CR>", opts)
-    vim.keymap.set("n", "gac", "<cmd>:lua require('textcase').current_word('to_camel_case')<CR>", opts)
-    vim.keymap.set("n", "gap", "<cmd>:lua require('textcase').current_word('to_pascal_case')<CR>", opts)
-    vim.keymap.set("n", "gad", "<cmd>:lua require('textcase').current_word('to_dash_case')<CR>", opts)
   end,
   keys = {
     "ga",
+    { "ga.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "x" }, desc = "Telescope" },
+    { "gas", "<cmd>:lua require('textcase').current_word('to_snake_case')<CR>" },
+    { "gac", "<cmd>:lua require('textcase').current_word('to_camel_case')<CR>" },
+    { "gap", "<cmd>:lua require('textcase').current_word('to_pascal_case')<CR>" },
+    { "gad", "<cmd>:lua require('textcase').current_word('to_dash_case')<CR>" },
   },
   lazy = true,
 }
