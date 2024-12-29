b51ddde3491fc6d99e2a234e80fed5b2f0c4cf82
charlie-dev-ios <py.yeronga@gmail.com>
2024-12-30 02:19:42 +0900

    telescopeでdotfilesも検索対象に含めるよう修正

--- vim/lua/plugins/telescope.lua
+++ vim/lua/plugins/telescope.lua
@@ -7,21 +7,40 @@ return {
         'nvim-tree/nvim-web-devicons',
     },
     keys = {
-      { "<Leader>fo", "<cmd>lua require'telescope.builtin'.find_files {}<CR>" },
-      { "<Leader>ff", "<cmd>lua require'telescope.builtin'.live_grep {}<CR>" },
-      { "<Leader>fh", "<cmd>lua require'telescope.builtin'.help_tags {}<CR>" },
+        { "<Leader>fo", "<cmd>lua require'telescope.builtin'.find_files {}<CR>" },
+        { "<Leader>ff", "<cmd>lua require'telescope.builtin'.live_grep {}<CR>" },
+        { "<Leader>fh", "<cmd>lua require'telescope.builtin'.help_tags {}<CR>" },
     },
     config = function()
         local actions = require("telescope.actions")
+        local telescopeConfig = require("telescope.config")
+
+        -- Clone the default Telescope configuration
+        local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+        -- I want to search in hidden/dot files.
+        table.insert(vimgrep_arguments, "--hidden")
+        -- I don't want to search in the `.git` directory.
+        table.insert(vimgrep_arguments, "--glob")
+        table.insert(vimgrep_arguments, "!**/.git/*")
+
         require("telescope").setup{
-          defaults = {
-            mappings = {
-              i = {
-                ["<esc>"] = actions.close
-              },
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<esc>"] = actions.close
+                    },
+                },
+                path_display = { "smart" },
+                -- `hidden = true` is not supported in text grep commands.
+                vimgrep_arguments = vimgrep_arguments,
             },
-            path_display = { "smart" },
-          }
+            pickers = {
+                find_files = {
+                    -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+                    find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+                },
+            },
         }
     end,
 }
