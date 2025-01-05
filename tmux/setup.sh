b57c68d737315eb5bb76310871b3eec624dc3b4b
charlie-dev-ios <py.yeronga@gmail.com>
2025-01-05 20:48:29 +0900

    tmuxのテーマをtokyo-nightに設定

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -14,3 +14,10 @@ if ! [ -d "${TPM_DIR}" ]; then
     git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
 fi
 
+THEME_DIR="$HOME/.tmux/themes"
+THEME_REPO="https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/tmux/tokyonight_moon.tmux"
+if ! [ -d "${THEME_DIR}" ]; then
+    mkdir $THEME_DIR
+fi
+curl -o "$THEME_DIR/tokyonight_storm.tmux" $THEME_REPO
+
