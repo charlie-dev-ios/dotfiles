c4ae5004e3fb0758276b542808677e4c58209067
charlie-dev-ios <py.yeronga@gmail.com>
2025-01-20 23:45:47 +0900

    tree-sitterにbashとregexを追加

--- vim/lua/plugins/nvim-treesitter.lua
+++ vim/lua/plugins/nvim-treesitter.lua
@@ -4,9 +4,8 @@ return {
     event = { "BufNewFile", "BufRead" },
     config = function () 
         local configs = require("nvim-treesitter.configs")
-
         configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "swift" },
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "swift", "bash", "regex"},
             sync_install = false,
             auto_install = true,
             highlight = { enable = true },
